a9c7c377b9149bfb03772a22f1ca478917e4e129
MANOJKUMAR7708 <manoj20560@gmail.com>
2025-03-14 12:00:17 +0530

    Update OddNumber.java

--- OddNumber.java
+++ OddNumber.java
@@ -1,5 +1,5 @@
 class OddNumber {
-public static void main(String args[]) {
+public static void main(String[] args) {
 System.out.print(" Odd Numbers  :");
 for (int i = 1; i <= 100; i++) {
 if (i % 2 != 0) {
